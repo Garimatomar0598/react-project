e8a86eaf9fb17a7ae5496453e4db34667c1b1b90
Garima Tomar <garimatomar55521@gmail.com>
2025-08-29 16:24:01 +0530

    build react animated website

---
+++ react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+  <h1 className='text-2xl text-white py-2 px-2 bg-blue-500'>Hey</h1> 
+    </>
+  )
+}
+
+export default App
